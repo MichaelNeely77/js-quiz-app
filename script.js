58f3a7c01f929f0ce8386eb24a4d8c41237050e0
Michael Neely <michael@michaelneely.net>
2020-07-01 23:47:13 -0400

    Added the first JS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const message = document.querySelector('.message');
+const game = {};
+const output = document.querySelector('.que');
+const output = document.querySelector('.next');
+
+nx.addEventListener('click', createQuestion);
+
+function createQuestion() {
+    nx.style.display = "none";
+}
 
