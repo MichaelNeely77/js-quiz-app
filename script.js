9ef2c37ccda6879254b37b8f5cfee6a83e29eb40
Michael Neely <michael@michaelneely.net>
2020-07-02 22:56:55 -0400

    Added the endpoint and started CreateQuestion funciotn

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const message = document.querySelector('.message');
 const game = {};
 const output = document.querySelector('.que');
-const output = document.querySelector('.next');
+const nx = document.querySelector('.next');
 
 nx.addEventListener('click', createQuestion);
+const url = 'https://script.google.com/macros/s/AKfycbyaXSH1Dsrd_JVNfSPWblP0vcwjcPCE3Y5Mgq9zbGUUVkW5_6Af/exec';
+fetch(url).then(function(res) {
+    return res.json()
+}).then(function(data) {
+    console.log(data.data);
+    game.total = data.data.length;
+    game.val = 0;
+    game.arr = data.data;
+    data.data.forEach(function(el) {
+        console.log(el);
+    })
+    createQuestion();
+})
 
 function createQuestion() {
     nx.style.display = "none";
