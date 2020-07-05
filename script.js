6cadcbe95363c1d6e6eb8979b2f581fb986a5699
Michael Neely <michael@michaelneely.net>
2020-07-04 22:49:15 -0400

    Added the new Google spreadsheet to the app

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const output = document.querySelector('.que');
 const nx = document.querySelector('.next');
 
 nx.addEventListener('click', createQuestion);
-const url = 'https://script.google.com/macros/s/AKfycbyaXSH1Dsrd_JVNfSPWblP0vcwjcPCE3Y5Mgq9zbGUUVkW5_6Af/exec';
+// const url = 'https://script.google.com/macros/s/AKfycbyaXSH1Dsrd_JVNfSPWblP0vcwjcPCE3Y5Mgq9zbGUUVkW5_6Af/exec';
+const url = 'https://script.google.com/macros/s/AKfycbzXIChKcDH5mI8RlbxeKsEmfYBWeTmq4UrCWhX77jRByiAIOV3E/exec';
 fetch(url).then(function(res) {
     return res.json()
 }).then(function(data) {
